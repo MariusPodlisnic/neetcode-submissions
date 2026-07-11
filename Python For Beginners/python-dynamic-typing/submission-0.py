9991a0b7608e2903ee88668405029d9e0eb1a5cd
Podlisnic Marius <marius.podlisnic@student.usv.ro>
2026-07-11 16:48:05 +0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 6.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "sixseven"
+print(type(variable))
+variable = [6,7]
+print(type(variable))
