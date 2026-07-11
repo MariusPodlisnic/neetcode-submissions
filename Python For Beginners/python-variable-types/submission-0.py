58b113f5a82f5774c9d0dbb7689d2f7b6c9a8a7b
Podlisnic Marius <marius.podlisnic@student.usv.ro>
2026-07-11 16:46:31 +0300

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,13 @@
+integer_type = 1
+float_type = 2.28
+bool_type = True
+string_type = "Joking"
+list_type = [6,7]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
